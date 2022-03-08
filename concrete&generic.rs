e17d143493b5bbebffee18235d456e3fb97cbe22
experiencei <ayelojahighbee01@gmail.com>
2022-03-08 13:32:59 +0100

    working with genric and concrete

---
+++ concrete&generic.rs
@@ -0,0 +1,70 @@
+//concrete implementation synthax
+
+trait Game {
+    fn name(&self) -> String;
+}
+
+
+enum BoardGame {
+    Chess,
+    Monopoly
+}
+
+
+enum VideoGame {
+    PlayStation,
+    Xbox,
+}
+
+impl Game for BoardGame {
+
+}
+
+impl Game for VideoGame {
+
+}
+
+struct PlayRoom<T: Game> {
+    game: T,
+}
+
+impl PlayRoom<BoardGame> {
+    pub fn cleanup(&self) {
+
+    }
+}
+
+let video_room = PlayRoom {
+    game: VideoGame::Xbox,
+}
+
+let board_room = PlayRoom {
+    game : BoardGame::Monopoly
+}
+
+board_room.cleanup();
+video_room.cleanup();    // we will get an error
+ 
+
+
+
+// generic implementation synthax
+
+struct Name<T: Trait1 + Trait2 , U: Trait3> {
+    field1: T,
+    field2: U,
+}
+
+impl<T: Trait1 + Trait2, U: Trait3> Name<T, U> {
+    fn func(&self, arg1: T , arg2: U) {}
+}
+
+// generic fn using the where keywords
+
+impl <T, U> Name<T, U>
+where 
+   T: Trait1 + Trait2, 
+   U: Trait3,
+   {
+     fn func(&self, arg1: T , arg2: U) {}
+   }

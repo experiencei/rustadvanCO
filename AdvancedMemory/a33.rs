a49be46d28f646dd097357d08804fa6ebf6e04b5
experiencei <ayelojahighbee01@gmail.com>
2022-03-08 17:00:46 +0100

    rust for lifetime and function

---
+++ AdvancedMemory/a33.rs
@@ -0,0 +1,13 @@
+fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if b.len() > a.len() {
+        b
+    } else {
+        a
+    }
+}
+
+fn main() {
+    let short = "hello";
+    let long = "this is a long message";
+    println!("{}", longest(short, long))
+}

b73ee0ba071eb230887b7dfb70262dee0339c55a
experiencei <ayelojahighbee01@gmail.com>
2022-03-09 07:33:42 +0100

    demo error activity continues

--- Error/ErrrorDEmo.rs
+++ Error/ErrrorDEmo.rs
@@ -14,6 +14,8 @@ enum PassError {
 }
 
 fn swipe_card() -> Result<SubwayPass, PassError> {
+  // dont needd to comment this OK but to test Err we comment it
+  Err(PassError::ReadError("Magstrip failed to read".to_owned()))
     OK(SubwayPass {
         id : 0,
         funds: 200,
@@ -30,6 +32,18 @@ fn use_pass(pass: &mut SubwayPass , cost: isize ) -> Result<() , PassError> {
         } else {
             pass.funds = pass.funds - cost;
             Ok(())
+        }
+    }
+}
+
+fn main() {
+    let pass_status = swipe_card().and_then(|mut pas| use_pass(&mut pass, 3));
+    match pass_status {
+        Ok(_) => println!("ok to board"),
+        Err(e) => match e {
+            PassError::ReadError(s) => (),
+            PassError::PassExpired => (),
+            
         }
     }
 }

0889efffbf59fcf59600afca42c736a18ffce260
experiencei <ayelojahighbee01@gmail.com>
2022-03-09 07:26:14 +0100

    demo on error with enum

--- Error/ErrrorDEmo.rs
+++ Error/ErrrorDEmo.rs
@@ -21,4 +21,15 @@ fn swipe_card() -> Result<SubwayPass, PassError> {
     })
 }
 
-fn use
+fn use_pass(pass: &mut SubwayPass , cost: isize ) -> Result<() , PassError> {
+    if Utc::now() > pass.expires {
+        Err(PassError::PassExpired)
+    } else {
+        if pass.fund - cost < 0 {
+            Err(PassError::InsufficientFunds(pass.funds))
+        } else {
+            pass.funds = pass.funds - cost;
+            Ok(())
+        }
+    }
+}

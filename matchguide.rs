6c83197d07b9b4fdc3d67d23616fdf2020314a68
experiencei <ayelojahighbee01@gmail.com>
2022-03-11 10:49:31 +0100

    using match guide instead of refunctioning

--- matchguide.rs
+++ matchguide.rs
@@ -65,3 +65,23 @@ fn main() {
         s => println!("stage {}" , stage),
     }
 }
+
+// example 4
+struct Vehicle {
+    km: usize,
+    year: usize
+}
+
+fn main() {
+    let car = {
+        km : 80_000,
+        year: 2022,
+    }
+    match car {
+        Vehicle {km , year} if km == 0 && year == 2020 => println!("new 2022 vehicle"),
+        Vehicle {km , ..} if km <= 50_000 => println!("under 50k km"),
+        Vehicle { km , ..} if km >= 80_000 => println!("at least 80k km"),
+        Vehicle { year , ..} if year == 2020 => println!("made in 2020"),
+        Vehicle { .. } => println!("other mileage"),
+    }
+}

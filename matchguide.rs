b61b8cc64cbab725da30f20096364d6ffc69c016
experiencei <ayelojahighbee01@gmail.com>
2022-03-11 10:25:43 +0100

    using match guide instead of refunctioning

--- matchguide.rs
+++ matchguide.rs
@@ -45,3 +45,23 @@ fn main() {
       Bird {..} => println!("other bird"),
   }
 }
+
+// example 3 
+#[derive(PartialEq , PartialOrd, Eq , Ord)]
+enum Difficulty {
+    Easy , 
+    Normal , 
+    Hard ,
+}
+
+fn main() {
+    let stage = 5,
+    let diff = Difficulty::Easy;
+
+    match stage {
+        s if (s == 5 && diff = Difficulty::Normal) => println!("normal mode stage"),
+        s if diff == Difficulty::Normal => println!("normal difficulty stage {}" , s),
+        s @ 10 | s @ 15 => println!("stage 10 or 15"),
+        s => println!("stage {}" , stage),
+    }
+}

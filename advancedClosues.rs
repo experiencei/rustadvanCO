fbabea21d098f5fa5b3ade422ba569190f733366
experiencei <ayelojahighbee01@gmail.com>
2022-03-12 16:36:37 +0100

    advanced closures for passing params

---
+++ advancedClosues.rs
@@ -0,0 +1,18 @@
+fn math(a: i32, b: i32 , op: Box<dyn Fn(i32, i32) -> i32) -> i32 {
+    op(a, b)
+}
+
+fn main() {
+    let name = "Ibrahim";
+    let add = Box::new(move |a , b| {
+        println!("adding a number for {}", name);
+    });
+    let sub = Box::new(|a, b| a - b);
+    let mul = Box::new(|a, b| a * b);
+    println!("{}" , math(2 , 3 , add));
+    println!("{}", math(2 , 3 , mul));
+    println!("{}", math(2 , 3 , sub);
+}
+
+// the Box is to constrict the size of the input
+// the move is to gain ownership of the name in to the closure, if there is no Box  we don't need to use the move keyword

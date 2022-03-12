d6ae76d17e58ab6bf0dd81997b1b0e318c650917
experiencei <ayelojahighbee01@gmail.com>
2022-03-12 18:11:55 +0100

    working on parrelel channel

--- channels.rs
+++ channels.rs
@@ -8,3 +8,18 @@ match reciever.recv() {
     Ok(msg) => println!("{}", msg),
     Err(e) => println!("{}", e)
 }
+
+
+// demo no2 
+use crossbeam_channel::unbounded;
+use std::thread;
+
+let (s , r) = unbounded();
+
+let handle = thread::spawn(move || match r.recv() {
+    Ok(msg) => println!("Thread {}", msg),
+    Err(e) => println!("{:?}", e),
+})
+
+s.send("Hello from main!")?;
+handle.join();

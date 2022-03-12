dfdf543382e265997ed266c122beabe8a5118378
experiencei <ayelojahighbee01@gmail.com>
2022-03-12 17:19:16 +0100

    working with threads and join to return joinhandle

--- threadDemo.rs
+++ threadDemo.rs
@@ -38,3 +38,20 @@ fn main() {
 }
 
 // always return JoinHandle<usize> and join spawning before termination from the main
+
+// Demo 3
+use std::thread;
+
+fn main() {
+    let data = vec!["a" , "b" , "c"];
+    let caps = thread::spawn(move || {
+        let data: Vec<char> = data.iter().map(|c| c.to_ascii_uppercase()).collect();
+    });
+
+    println!("waiting on value..");
+
+    match caps.join() {
+        Ok(n) => println!("value: {:?}", n),
+        Err(e) => println!("error: {:?}", e),
+    }
+}

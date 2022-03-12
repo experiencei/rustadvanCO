98f59e9fab2d0fff8bfa0400c879eb6c15f93098
experiencei <ayelojahighbee01@gmail.com>
2022-03-12 20:53:37 +0100

    smart pointer usage with multithreading and single

---
+++ smartPointer.rs
@@ -0,0 +1,26 @@
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct Vehicle {
+    vin : String,
+}
+
+#[derive(Debug)]
+struct Door {
+    vehicle: Rc<Vehicle>,
+}
+
+let car = Rc::new(Vehicle {
+    vin : "123".to_owned(),
+});
+
+let left_door = Door {
+    vehicle: Rc::clone(&car),
+};
+let right_door = Door { 
+    vehicle: Rc::clone(&car)
+}
+
+drop(car);
+
+println!("vehicle = {:?}", left_door.vehicle);
